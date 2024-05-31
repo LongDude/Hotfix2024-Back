e6850e49b6316ebbc5630bbc0b253a889514098b
LiveisFPV <Nasonovnicolai@gmail.com>
2024-05-31 15:58:05 +0300

    registration

--- PlywoodTickets/PlywoodTickets/settings.py
+++ PlywoodTickets/PlywoodTickets/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/personalLocker/login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
