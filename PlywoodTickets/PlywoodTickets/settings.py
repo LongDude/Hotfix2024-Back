49b0862587812382391408dba83d4123cd491843
LiveisFPV <Nasonovnicolai@gmail.com>
2024-05-31 12:35:17 +0300

    Createdapps

--- PlywoodTickets/PlywoodTickets/settings.py
+++ PlywoodTickets/PlywoodTickets/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
+    'registration.apps.RegistrationConfig',
+    'personalLocker.apps.PersonallockerConfig',
+    'mainpage.apps.MainpageConfig',
 ]
 
 MIDDLEWARE = [
