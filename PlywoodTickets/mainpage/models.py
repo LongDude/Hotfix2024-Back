334e4c12a990a285abf806f3164207629e9a5f9f
LiveisFPV <Nasonovnicolai@gmail.com>
2024-05-31 16:47:41 +0300

    aaaa

--- PlywoodTickets/mainpage/models.py
+++ PlywoodTickets/mainpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Flights(models.Model):
+    id_flights=models.BigIntegerField(primary_key=True)
+    id_plane=models.BigIntegerField()
+    country_plan=models.TextField()
+    to_city=models.TextField()
+    from_city=models.TextField()
+    start=models.TimeField()
+    end=models.TimeField()
+    price=models.DecimalField(decimal_places=16,max_digits=32)
+    
