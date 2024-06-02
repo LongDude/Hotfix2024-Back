5d4072047f6728fb221254e7e629ff4c8bf05519
phoenix381 <phoenix0381@gmail.com>
2024-06-02 15:51:05 +0300

    ml text respomse

--- ml/fix_mean.py
+++ ml/fix_mean.py
@@ -1,50 +1,16 @@
 import pandas as pd
 
-# excel_data = pd.ExcelFile(path)
 df = pd.read_excel('data/mean.xlsx')
 
-# df.info()
+print(df[df[4] == 0].shape[0])
+df.reset_index(drop=True, inplace=True)
 
-df = df[(df[4] > 0)]
+# df[4] = df.groupby([0,1])[4].apply(lambda x: x.interpolate())
+df = df.groupby([0,1])[4].apply(
+            lambda group: group.interpolate(method='spline', order=3)
+        )
+# for label,group in df.groupby([0,1]):
+	# print(b)
 
-# df.info()
-
-X = df.drop(columns=[4])
-y = df[4]
-
-print(f"{X.shape=} {X=}")
-X.info()
-print(f"{y.shape=} {y=}")
-y.info()
-
-
-# exit()
-
-import tensorflow as tf
-import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-model = Sequential([
-    Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
-    Dropout(0.2),
-    Dense(64, activation='relu'),
-    Dropout(0.3),
-    Dense(32, activation='relu'),
-    Dense(16, activation='relu'),
-    Dense(1)
-])
-
-model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(), metrics=['mae'])
-model.fit(X_train, y_train, epochs=30, validation_split=0.1, batch_size=1024, )
-
-y_pred = model.predict(X_test)
-
-mse = mean_squared_error(y_test, y_pred)
-print("Mean Squared Error:", mse)
+print(df[df[4] == 0].shape[0])
+df.to_excel('data/mean_fixed.xlsx', index=False)
