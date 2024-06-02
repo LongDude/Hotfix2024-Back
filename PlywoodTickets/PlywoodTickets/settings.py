39021a848b6d8f4ace0dd244bd1040afc2678b18
LiveisFPV <Nasonovnicolai@gmail.com>
2024-06-02 10:39:05 +0300

    pagination start

--- PlywoodTickets/PlywoodTickets/settings.py
+++ PlywoodTickets/PlywoodTickets/settings.py
@@ -54,6 +54,8 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': ( 
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
